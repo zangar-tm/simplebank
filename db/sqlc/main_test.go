d8daaca705d3f20a6cd7dda18828a6f6fd64e9f1
zangar-tm <zake.taubay@gmail.com>
2021-10-04 18:14:18 +0600

    git:lib/pq, testing

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,7 +1,28 @@
 package db 
 
+import (
+	"database/sql"
+	"testing"
+	"os"
+	"log"
+
+	_"github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
 var testQueries *Queries
 
 func TestMain(t *testing.M) {
 	conn, err := sql.Open(dbDriver,dbSource)
+	if err != nil {
+		log.Fatal("can't connect to db:",err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
 }
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/zangar-tm/simple_bank
+module github.com/zangar-tm/simplebank
 
 go 1.16
+
+require github.com/lib/pq v1.10.3
