c5e11d3d89c6c58449bc266c1793f436455e57a9
zangar-tm <zake.taubay@gmail.com>
2021-10-06 15:33:57 +0600

    feature:tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,28 +1,30 @@
-package db 
+package db
 
 import (
 	"database/sql"
-	"testing"
-	"os"
 	"log"
+	"os"
+	"testing"
 
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
+
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver,dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
-		log.Fatal("can't connect to db:",err)
+		log.Fatal("can't connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
-
